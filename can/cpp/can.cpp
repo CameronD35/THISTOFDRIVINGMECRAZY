bc30c759c3545c1d49e845aa7f7c68de29817b4b
CameronD35 <camster09@ca.rr.com>
2025-06-04 18:49:13 -0700

    Added some comments and removed extra code.

--- can/cpp/can.cpp
+++ can/cpp/can.cpp
@@ -12,7 +12,9 @@ class TofSensor {
     public:
         TofSensor(asio::io_context& io, const std::string& port = "COM6") // might need to change port, should be this since running off windows os?
             : serial_(io, port), timer_(io) {
+
                 // Config of serial ports for SLCAN
+                // 1Mbps, 8 character bits, no stop bits, no parity, no flow control
                 serial_.set_option(serial_port::baud_rate(1000000));
                 serial_.set_option(serial_port::character_size(8));
                 serial_.set_option(serial_port::stop_bits(serial_port::stop_bits::one));
@@ -48,17 +50,23 @@ class TofSensor {
             std::array<char, 256> read_buf_;
             std::string read_buffer_;
 
+            // sends command via Lawicel CAN/CAN232 Protocol
+            // Protocol pdf: https://www.canusb.com/files/canusb_manual.pdf
             void send_command(const std::string& cmd) {
                 asio::write(serial_, asio::buffer(cmd.c_str(), cmd.size()));
 
                 std::cout << "sending " << cmd << std::endl;
             }
 
+            // create RTR (remote frame) for Lawicel CAN/CAN232 protocol
             void send_remote_frame(uint32_t can_id, int data_length){
                 std::ostringstream cmd;
+                // converts can_id to hexadecimal with length of 3 and appends data length
+                // data length will always be zero since the TOF does not read data
                 cmd << "r" << std::hex << std::setw(3) << std::setfill('0') << can_id << data_length << "\r";
                 send_command(cmd.str());
             }
+
 
             void handleRead(const boost::system::error_code& ec, size_t bytes) {
                 if (!ec) {
@@ -95,14 +103,14 @@ class TofSensor {
 
 
                     for (size_t i = 0; i < new_frame.size(); i += 2) {
-                        uint16_t byte;
+                        uint16_t formatted_byte;
+                        
+                        std::string raw_byte = new_frame.substr(i, 2);
 
-                        std::string something = new_frame.substr(i, 2);
+                        std::istringstream(raw_byte) >> std::hex >> formatted_byte;
 
-                        std::istringstream(something) >> std::hex >> byte;
-
-                        std::cout << something << " | " << byte << " | ";
-                        data.push_back(byte);
+                        std::cout << raw_byte << " | " << formatted_byte << " | ";
+                        data.push_back(formatted_byte);
                     }
 
                     if (data.size() >= 8) {
@@ -120,30 +128,23 @@ class TofSensor {
 
             void interpretMeasurements(const std::vector<uint16_t>& meas) {
 
-                // for (auto i = meas.begin(); i != meas.end(); i++) {
-                //     std::cout << *i << " ";
-                // }
-
-                // std::cout << (meas[0] << 16 | meas[1] << 8 | meas[2]) << std::endl;
-
-                unsigned char test = meas[0];
-                unsigned char testtwo = meas[3];
-
-                // std::cout << test << " | " << testtwo << std::endl;
-
+                // measured in mm
                 double distance = (meas[0] << 16) | (meas[1] << 8) | (meas[2]);
+
+                // convert from mm to m
                 distance = distance / 1000.0;
 
+                // measured in LSB
                 double amplitude = (meas[3] << 8) + (meas[4]);
-                amplitude = amplitude;
 
+                // In percentage (0-100%)
                 uint8_t signal_quality = meas[5];
+
+                // see https://broadcom.github.io/AFBR-S50-API/group__argus__status.html#gaaabdaf7ee58ca7269bd4bf24efcde092
+                // for error codes
                 int16_t status = (meas[6] << 8 | meas[7]);
 
-                // if (status >= 0x8000) {
-                //     status -= 0x10000;
-                // }
-
+                // output values
                 std::cout << "Distance: " << distance << " | Amplitude: " << amplitude << " | Status: " << status << std::endl;
 
             }
@@ -152,15 +153,28 @@ class TofSensor {
 int main (int argc, char** argv) {
     try {
 
+        // create IO context allowing for read/write operations
         asio::io_context io;
+
+        // creates TOF sensor instance
         TofSensor sensor(io);
+
+        // begin measurements (send 0x08 to TOF)
         sensor.startMeasurements();
+
+        // start reading from 0x1C address 
         sensor.asyncReadStart();
 
+        // open seperate thread
         std::thread([&io]() { io.run(); }).detach();
         std::this_thread::sleep_for(std::chrono::seconds(20));
 
+        // stop measurements (send 0x09 to TOF)
         sensor.stopMeasurements();
+        
+        // add slight delay to ensure proper stopping
+        Sleep(200);
+
         io.stop();
 
     }
